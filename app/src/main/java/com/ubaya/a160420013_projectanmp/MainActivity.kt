a6a3ba8b605b3709547247ce83a70cb22a1fdf0c
alvinfersus <alvinfersus@gmail.com>
2023-05-08 20:54:59 +0700

    initial commit -> UI Done

---
+++ app/src/main/java/com/ubaya/a160420013_projectanmp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ubaya.a160420013_projectanmp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as
+                    NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
+    }
+}
